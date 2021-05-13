fc24c38feaecb06cc3180d3ea9fcf3b0d5ca0512
Zachattack98 <bakugan0498@yahoo.com>
2021-05-13 02:20:45 -0700

    Error (LRU Seg fault): either new vector is wrong, should've provided LRU_position in struct, or wrong modulus

--- README.md
+++ README.md
@@ -2,5 +2,6 @@ Team Members: Zachary Hill and Arturo Alvarado
 
 Email Address: zhill003@ucr.edu
 
-We are currently having trouble creating lines of code that implement FIFO and LRU correctly.
+- We had trouble creating lines of code that implement FIFO and LRU correctly.
+- While the first few lines in FIFO were sufficient, the miss counter was incrementing too many times after all cache blocks were filled.
 
--- cache.cpp
+++ cache.cpp
@@ -35,12 +35,12 @@ struct cache_entry
    int blockCount;
 };
 
-main(int argc, char **argv)
+int main(int argc, char **argv)
 {
    if (argc < 4)
    {
-      cout << "Insufficient input argugemnts" << endl;
-      exit(-1);
+      cout << "Insufficient input arguments less than 4" << endl;
+      exit(-1); // error code:wq
    }
    size_t associativity = stoi(argv[1]);
    size_t cache_size = stoi(argv[2]);     // Cache suze
@@ -58,9 +58,9 @@ main(int argc, char **argv)
    unsigned long long read_line;
    unsigned long long index_offset = 0;
 
+   int printCnt2 = 1;
    if (replacement_algorithm == "LRU")
    {
-      int tagCounter = 1;
       while (std::cin >> std::hex >> read_line)
       {
          total++;
@@ -76,125 +76,13 @@ main(int argc, char **argv)
          // Now we have offset, so can and it with index to find specific index given inputs
          index = index & index_offset;
 
-         vector<int> metadata_holder;
+         uint32_t position = index * associativity;
 
-         // check for miss
-         if (associativity == 1)
-         {
-            if (cache[index].tag != tag)
-            {
-               cache[index].tag = tag; // replacement
-               miss++;
-            }
-            else
-            {
-               ;
-            }
-         }
-         else
-         {
-            int i;
+         vector<int> metadata_holder;  //vector for storing metadata
 
-               for (i = 0; i < associativity; i++) {
-                  if (cache[index + i].valid == false && cache[index + i].tag != tag) {
-                  miss++;
-                  cache[index + i].tag = tag;
-                  cache[index + i].valid = true;
-                  
-                  for (int j = 0; j < associativity; j++) {
-                     metadata_holder[index + j]++;  //increment all values in metadata of each index
-                  }
-
-                  if (tagCounter < 101)
-                  {
-                     cout << "line: " << tagCounter << ", tag: " << tag << ", index: " << index << ", missed, wrote to the " << cache[index].blockCount << "th cache block." << endl;
-                  }
-                  if (cache[index].blockCount < associativity - 1)
-                  {
-                     cache[index].blockCount++;
-                  }
-                  break;
-               }
-               else if (cache[index + i].tag == tag)
-               {
-                  if (tagCounter < 101)
-                  {
-                     cout << "line: " << tagCounter << ", tag: " << tag << ", index: " << index << ", cache hit." << endl;
-                  }
-                  break;
-               }
-            }
-
-            int replacementPosition;
-            int fifo = 0;
-            bool validMeta = 1;
-
-            if (i >= associativity)
-            {
-               // Done with loop, we failed to find a match
-               // and failed to find available position
-               // Need replacement
-               if (cache[index].FIFO_position == fifo) {
-                  miss = miss + 2;
-               }
-               else {
-                  fifo = cache[index].FIFO_position;
-               }
-
-               //replacementPosition = cache[index].FIFO_position;
-
-               //check if all metadata indexes are equal
-               for (int j=1; j < associativity; j++) {
-                  if(metadata_holder[index + j - 1] != metadata_holder[index + j])
-                     validMeta = 0;
-               }
-
-               if(validMeta == 1) {
-                  metadata_holder[0] == 0;   //set first index to zero
-                  replacementPosition = metadata_holder[index + j];
-               }
-               else {
-                  //find highest valued index in metadata
-                  for (int j=1; j < associativity; j++) {
-                     if(metadata_holder[index + j - 1] >= metadata_holder[index + j]) {
-                        metadata_holder[index + j] = 0;
-                        replacementPosition = metadata_holder[index + j];
-                     }
-                  }
-               }
-
-               if (tagCounter < 101)
-               {
-                  cout << "line: " << tagCounter << ", tag: " << tag << ", index: " << index << ", missed, wrote to the " << replacementPosition << "th cache block." << endl;
-                  //cout << "miss: " << miss << endl;
-                  //cout << "total: " << total << endl;
-               }
-               cache[index].FIFO_position = (cache[index].FIFO_position + 1) % associativity;
-            }
-            else {
-               replacementPosition = i;
-            }
-            
-            cache[index + replacementPosition].tag = tag;
-            cache[index + replacementPosition].valid = true;
-            tagCounter++;
-         }
-      }
-   }
-   /************************************/
-
-   if (replacement_algorithm == "FIFO")
-   {
-      int first_in_tracker = 0;
-      int tagCounter = 1;
-      while (cin >> hex >> read_line)
-      {
-         total++;
-
-         uint32_t tag = read_line >> ((int)log2(cache_size) - (int)log2(associativity));
-         uint32_t index = (read_line >> 4) & (number_of_sets - 1);
-
-         vector<int> index_holder;
+         int i;
+         int idx;
+         bool validMeta = 1;
 
          // Check whether the current cache line is a hit or miss
          // in the cache data structure
@@ -210,114 +98,167 @@ main(int argc, char **argv)
             if (cache[index].tag != tag)
             {
                cache[index].tag = tag; // replacement
-               miss++;  // increment the miss counter
+               miss++;                 // increment the miss counter
+            }
+         }
+
+         else
+         {
+            for (i = 0; i < associativity && cache[position + i].tag != tag && cache[position + i].valid == true; i++)
+            {
+               ;
+            }
+
+            for (int j = 0; j < associativity; j++) {
+               metadata_holder[position + j]++;  //increment all values in metadata of each index
+            }
+
+            if (i >= associativity || cache[position + i].valid == false)
+            {
+               miss++;
+               if (printCnt2 < 1000)
+               {
+                  cout << "line: " << printCnt2 << ", tag: " << tag << ", index: " << index << ", cache hit." << endl;
+
+               }
+            }
+
+            // i had reached max, and no hit found;
+            if (i >= associativity)
+            {
+               if (printCnt2 < 1000)
+               {
+                  cout << "Set is filled" << endl;
+                  cout << "line: " << printCnt2 << ", tag: " << tag << ", index: " << index << ", missed, wrote to the " << cache[index].FIFO_position << "th cache block." << endl;
+               }
+               //idx = cache[position].FIFO_position;
+
+               //check if all metadata indexes are equal
+               for (int j=1; j < associativity; j++) {
+                  if(metadata_holder[position + j - 1] != metadata_holder[position + j]) {
+                     validMeta = 0;
+                     break;
+                  }
+                  else {
+                     validMeta = 1;
+                  }
+               }
+
+               if(validMeta == 1) {
+                  metadata_holder[0] == 0;   //set first index to zero
+                  idx = metadata_holder[position + i];
+               }
+               else {
+                  //find highest valued index in metadata
+                  for (int j=1; j < associativity; j++) {
+                     if(metadata_holder[position + j - 1] >= metadata_holder[position + j]) {
+                        metadata_holder[position + j] = 0;
+                        idx = metadata_holder[position + j];
+                     }
+
+                     metadata_holder[position + j] = (metadata_holder[position + j] + 1) % associativity;
+                  }
+               }
+
+               //cache[position].FIFO_position = (cache[position].FIFO_position + 1) % associativity;
             }
             else
             {
-               ;
+               idx = i;
+               // i has reached an empty slot
+               if (printCnt2 < 1000)
+               {
+                  cout << "line: " << printCnt2 << ", tag: " << tag << ", index: " << index << ", missed, wrote to the " << cache[index].blockCount << "th cache block." << endl;
+               }
+                  
+            }
+            cache[position + idx].tag = tag;
+            cache[position + idx].valid = true;
+            cache[position].blockCount++;
+         }
+         printCnt2++;
+      }
+   }
+   //
+   int printCnt = 1;
+   if (replacement_algorithm == "FIFO")
+   {
+      int first_in_tracker = 0;
+      int tagCounter = 1;
+      while (std::cin >> std::hex >> read_line)
+      {
+         total++;
+
+         uint32_t tag = read_line >> ((uint32_t)log2(cache_size) - (uint32_t)log2(associativity));
+         uint32_t index = (read_line >> 4) & (number_of_sets - 1);
+         uint32_t position = index * associativity;   //index needs to change based on associativity to allow the correct number of misses
+
+         int i;
+         int idx;  //index to write the cache
+
+         if (associativity == 1)
+         {
+            if (cache[index].tag != tag)
+            {
+               cache[index].tag = tag; // replacement
+               miss++;                 // increment the miss counter
             }
          }
          else
          {
-            int i;
-            //int idx;
-            //for (i = 0; i < associativity && cache[index + i].valid == false && cache[index + i].tag != tag; i++) {;}
-               
-               /*if ( i == associativity || cache[index+i].valid == false ) { miss++; }*/
-               
-
-               /*****if (cache[index + i].valid == false && cache[index + i].tag != tag)
-               {
-                  cache[index + idx].tag = tag;
-                  cache[index + idx].valid = true;
-               }
-
-               cache[index].FIFO_position += 1;
-
-               if (cache[index].FIFO_position == associativity)
-               {
-                  cache[index].FIFO_position = 0;
-               }*****/
-
-               /*if (i >= associativity) {
-                  idx = cache[index].FIFO_position;
-                  cache[index].FIFO_position = (cache[index].FIFO_position + 1) % associativity;
-               }
-               else{
-                  idx = i;
-               }
-
-               cache[index + idx].tag = tag;
-               cache[index + idx].valid = true;*/
-
-               for (i = 0; i < associativity; i++) {
-                  if (cache[index + i].valid == false && cache[index + i].tag != tag) {
-                  miss++;
-                  cache[index + i].tag = tag;
-                  cache[index + i].valid = true;
-                  if (tagCounter < 101)
-                  {
-                     cout << "line: " << tagCounter << ", tag: " << tag << ", index: " << index << ", missed, wrote to the " << cache[index].blockCount << "th cache block." << endl;
-                  }
-                  if (cache[index].blockCount < associativity - 1)
-                  {
-                     cache[index].blockCount++;
-                  }
-                  break;
-               }
-               else if (cache[index + i].tag == tag)
-               {
-                  if (tagCounter < 101)
-                  {
-                     cout << "line: " << tagCounter << ", tag: " << tag << ", index: " << index << ", cache hit." << endl;
-                  }
-                  break;
-               }
-            }
-
-            int replacementPosition;
-            int fifo = 0;
-
-            if (i >= associativity)
+            for (i = 0; i < associativity && cache[position + i].tag != tag && cache[position + i].valid == true; i++)
             {
-               // Done with loop, we failed to find a match
-               // and failed to find available position
-               // Need replacement
-               if (cache[index].FIFO_position == fifo) {
-                  miss = miss + 2;
-               }
-               else {
-                  fifo = cache[index].FIFO_position;
-               }
-
-               replacementPosition = cache[index].FIFO_position;
-
-               if (tagCounter < 101)
+               // Finds the value of i we need
+               ;
+            }
+            if (i >= associativity || cache[position + i].valid == false)
+            {
+               miss++;
+               if (printCnt < 1000)
                {
-                  cout << "line: " << tagCounter << ", tag: " << tag << ", index: " << index << ", missed, wrote to the " << replacementPosition << "th cache block." << endl;
-                  //cout << "miss: " << miss << endl;
-                  //cout << "total: " << total << endl;
+                  cout << "line: " << printCnt << ", tag: " << tag << ", index: " << index << ", cache hit." << endl;
                }
-               cache[index].FIFO_position = (cache[index].FIFO_position + 1) % associativity;
             }
-            else {
-               replacementPosition = i;
-            }
-            
-            cache[index + replacementPosition].tag = tag;
-            cache[index + replacementPosition].valid = true;
-            tagCounter++;
+            //else //since we entering this loop more often, the miss counter was higher than it should be
+            //{
+               // i had reached max, and no hit found;
+               if (i >= associativity)
+               {
+                  if (printCnt < 1000)
+                  {
+                     cout << "Set is filled" << endl;
+                     cout << "line: " << printCnt << ", tag: " << tag << ", index: " << index << ", missed, wrote to the " << cache[index].FIFO_position << "th cache block." << endl;
+                  }
+                  idx = cache[position].FIFO_position;
+                  //cache[position].tag = tag;
+                  cache[position].FIFO_position = (cache[position].FIFO_position + 1) % associativity;
+               }
+               else
+               {
+                  idx = i;
+                  // i has reached an empty slot
+                  if (printCnt < 1000)
+                  {
+                     cout << "line: " << printCnt << ", tag: " << tag << ", index: " << index << ", missed, wrote to the " << cache[index].blockCount << "th cache block." << endl;
+                  }
+                  
+               }
+               cache[position + idx].tag = tag;
+               cache[position + idx].valid = true;
+               cache[position].blockCount++;
+            //}
          }
+         printCnt++;
       }
    }
    cout << "miss: " << miss << endl;
    cout << "total: " << total << endl;
    float miss_rate = (float)miss / float(total);
 
-   cout << "associativity:      " << associativity << endl;
-   cout << "cache size:         " << cache_size << endl;
-   cout << "replacement policy: " << argv[3] << endl;
-   cout << "miss rate:          " << fixed << setprecision(2) << (miss_rate * 100.0) << endl;
+   std::cout << "associativity:      " << associativity << std::endl;
+   std::cout << "cache size:         " << cache_size << std::endl;
+   std::cout << "replacement policy: " << argv[3] << endl;
+   std::cout << "miss rate:          " << std::fixed << std::setprecision(2) << (miss_rate * 100.0) << std::endl;
 
    return 0;
 }
